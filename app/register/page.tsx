98ace316377e37ec00398ca65839d73deab9daca
Emir <_emirismailov750@gmail.com>
2026-01-07 22:46:17 +0500

    fix: change supabase import path

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { supabase } from '@/lib/supabase'; // Проверь, что путь к supabase верный
+import { supabase } from '../lib/supabase'; // Проверь, что путь к supabase верный
 import { useRouter } from 'next/navigation';
 
 export default function RegisterPage() {

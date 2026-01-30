f4e089e62ee05e10fa3fafcb2a1982cf76dde3c5
Emir <_emirismailov750@gmail.com>
2026-01-30 18:54:23 +0500

    Completely disable immersive mode in Android
    - Fixed next.config.mjs syntax error
    - Removed immersive mode from MainActivity.java onWindowFocusChanged
    - Status bar and navigation will now always be visible
    - Users can see time, notifications and navigation buttons

--- android/app/src/main/java/com/raddell/app/MainActivity.java
+++ android/app/src/main/java/com/raddell/app/MainActivity.java
@@ -76,24 +76,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            // Скрываем системные бары при фокусе для полного immersiv
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                WindowInsetsController controller = getWindow().getInsetsController();
-                if (controller != null) {
-                    controller.hide(WindowInsets.Type.systemBars());
-                    controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-                }
-            } else {
-                int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                          View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                          View.SYSTEM_UI_FLAG_FULLSCREEN |
-                          View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                
-                getWindow().getDecorView().setSystemUiVisibility(flags);
-            }
-        }
+        // НЕ скрываем системные бары - отключаем immersive mode
+        // Пользователи должны видеть статус-бар и навигацию всегда
     }
 }
